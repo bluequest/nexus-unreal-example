bb5fa38652025aca2d6fa062f2e48fc3693807d2
Voltaire Stephen Medina <vmedina@punyhuman.com>
2023-04-12 15:01:21 -0400

    Creator support menu styling changes. Add referral progress bar and player referral code section

--- NexusSampleProject/Source/NexusSampleProject/Public/UI/CreatorSupportUserWidget.h
+++ NexusSampleProject/Source/NexusSampleProject/Public/UI/CreatorSupportUserWidget.h
@@ -6,8 +6,10 @@
 #include "UI/NexusSampleProjectBaseWidget.h"
 #include "CreatorSupportUserWidget.generated.h"
 
+class UTextBlock;
 class UButton;
 class UEditableTextBox;
+class UProgressBar;
 
 /**
  * Widget used to utilize Nexus Support-A-Creator functionality 
@@ -25,17 +27,30 @@ protected:
 	void NativeConstruct() override;
 
 private:
+	
+	/** UTextBlock for containing the player's referral code */
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* PlayerReferralCode;
+
 	/** UButton for returning to game */
 	UPROPERTY(meta = (BindWidget))
 	UButton* BackButton;
 
-	/** UButton for submitting a creator code */
+	/** UButton for submitting a referral code */
 	UPROPERTY(meta = (BindWidget))
 	UButton* SubmitButton;
 
-	/** UEditableText for submitting a creator code */
+	/** UButton for copying the player's referral code */
+	UPROPERTY(meta = (BindWidget))
+	UButton* CopyCodeButton;
+	
+	/** UEditableText for submitting a referral code */
 	UPROPERTY(meta = (BindWidget))
 	UEditableTextBox* CreatorCodeInputTextBox;
+
+	/** UProgressBar the referral progress bar */
+	UPROPERTY(meta = (BindWidget))
+	UProgressBar* ReferralProgressBar;
 
 	/** Callback for when the back button is pressed */
 	UFUNCTION(BlueprintCallable, Category = "Creator Support Menu Buttons")
