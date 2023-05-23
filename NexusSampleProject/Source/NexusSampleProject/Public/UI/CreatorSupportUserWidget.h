e0f1bef615085d813df7f9e690f0b05e2211b2c3
Voltaire Stephen Medina <vmedina@punyhuman.com>
2023-05-23 13:51:53 -0400

    Add input validation to creator code, increase creator code char limit to 7, add public key

--- NexusSampleProject/Source/NexusSampleProject/Public/UI/CreatorSupportUserWidget.h
+++ NexusSampleProject/Source/NexusSampleProject/Public/UI/CreatorSupportUserWidget.h
@@ -5,6 +5,9 @@
 #include "CoreMinimal.h"
 #include "NexusPrototype.h"
 #include "GameFramework/SaveGame.h"
+#include "Generated/AttributionAPI.h"
+#include "Generated/ReferralAPI.h"
+#include "NexusShared.h"
 #include "UI/NexusSampleProjectBaseWidget.h"
 #include "UI/LinkAccountUserWidget.h"
 #include "CreatorSupportUserWidget.generated.h"
@@ -28,6 +31,9 @@ public:
 	UPROPERTY(BlueprintReadOnly, Category = "Save Game")
 	UNexusSampleProjectSaveGame* SaveGameInstance;
 
+	UPROPERTY(BlueprintReadOnly, Category = "Creator Support Menu")
+	TArray<FString> CreatorCodeList;
+
 protected:
 
 	void NativeConstruct() override;
@@ -48,10 +54,6 @@ protected:
 	UPROPERTY(EditAnywhere, Category = "Creator Support Menu")
 	int32 CreatorCodeCharacterLimit = 6;
 
-	/** #TODO Remove me when Unreal SDK is in. Delegate used for when retrieving cat facts completes */
-	NexusSDK::FOnGetCatFactsComplete OnGetGatFactsCompleteDelegate;
-	// #TODO ~End remove me
-
 private:
 
 	/** Callback for when the back button is pressed */
@@ -66,12 +68,24 @@ private:
 	UFUNCTION(BlueprintCallable, Category = "Creator Support Menu Buttons")
 	void OnTextChanged(const FText& Text);
 
-	/** #TODO Remove me once Unreal SDK is in. NexusAPI - Callback for when Cat Facts are retrieved */
-	void OnGetCatFactsComplete(const NexusSDK::FGetCatFactsResponse& Response);
-
 	/** Callback for when saving the save game to slot completes */
 	void OnAsyncSaveGameToSlotComplete(const FString& SlotName, const int32 UserIndex, bool bWasSuccessful);
 
 	/** Callback for when loading the save game to slot completes */
 	void OnAsyncLoadGameFromSlotComplete(const FString& SlotName, const int32 UserIndex, USaveGame* OutSaveGame);
+
+	/** Callback for when GetCreatorsCompletes successfully */
+	void OnGetCreatorsComplete(const FNexusAttributionGetCreators200Response& Response);
+
+	/** Callback for when GetCreatorsCompletes fails */
+	void OnGetCreatorsError(int32 ErrorCode);
+
+	/** Callback for when OnGetReferralInfoByPlayerId completes, and returns a 200 response */
+	void OnGetReferralInfoByPlayerId200ResponseComplete(const FNexusReferralGetReferralInfoByPlayerId200Response& Response);
+
+	/** Callback for when OnGetReferralInfoByPlayerId completes, and returns a 400 response */
+	void OnGetReferralInfoByPlayerId400ResponseComplete(const FNexusReferralReferralError& Response);
+
+	/** Callback for when GetCreatorCodeCompletes fails */
+	void OnGetCreatorCodeError(int32 ErrorCode);
 };
